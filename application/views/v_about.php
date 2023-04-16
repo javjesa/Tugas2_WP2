d0ebcbcb7908deb92f38e7c31767bc54d3d2f295
javjesa <javaekasabdaalam@gmail.com>
2023-04-16 15:30:44 +0700

    tugas2_WP2

---
+++ application/views/v_about.php
@@ -0,0 +1,21 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Java Eka Sabda Alam</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Kramat Sentiong, Jakarta Pusat</li>
+    </ul>
+    <h4>Tempat/Tanggal Lahir</h4>
+    <ul type="none">
+        <li>Jakarta, 22 Januari 2001</li>
+    </ul>
+    <h4>Hobi</h4>
+    <ul type="square">
+        <li>Cari Duit</li>
+        <li>Do'a Minta Dikasih Kaya Raya</li>
+    </ul>
+</section>
